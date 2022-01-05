d3d8a264d61bb351f69513c15cdfccf20f211c9c
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-01-05 21:05:42 +0100

    Initial Commit

---
+++ Showroom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Showroom
+//
+//  Created by Aitor Zubizarreta Perez on 05/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Showroom/ShowroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowroomApp.swift
+//  Showroom
+//
+//  Created by Aitor Zubizarreta Perez on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShowroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
